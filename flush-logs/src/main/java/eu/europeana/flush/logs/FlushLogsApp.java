7abe1003b7102a23f03ab8853f4870f0eb6760b0
srishti <srishti.singh@europeana.eu>
2023-07-24 16:17:27 +0200

    EA-3451 add another path

--- flush-logs/src/main/java/eu/europeana/flush/logs/FlushLogsApp.java
+++ flush-logs/src/main/java/eu/europeana/flush/logs/FlushLogsApp.java
@@ -38,8 +38,8 @@ public class FlushLogsApp {
             if (uri.toString().contains("!")) {
                 final Map<String, String> env = new HashMap<>();
                 final String[] array = uri.toString().split("!");
-                final FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), env);
-                final Path path = fs.getPath(array[1]);
+                final FileSystem fs = FileSystems.newFileSystem(URI.create(array[0] + array[1]), env);
+                final Path path = fs.getPath(array[2]);
                 LOG.info("path {}", path);
                 Stream<Path> pathStream = Files.list(Paths.get(uri));
                 pathStream.forEach(FlushLogsApp::print);
